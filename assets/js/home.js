f253ac36d6a1f3731f47e4f3c4835a18587efdd2
Ayush <ayush.kanduri@gmail.com>
2022-08-27 17:03:50 +0530

    Created Toast Notifications

--- assets/js/home.js
+++ assets/js/home.js
@@ -25,15 +25,88 @@
 		console.log(error);
 	}
 	try {
+		function notification(END, type) {
+			if (type === "error") END = true;
+			if (type === "success") END = false;
+
+			const toastContainer = document.querySelector(".toast-container");
+			const toast = toastContainer.querySelector(".toast").cloneNode(true);
+			toast.style.display = "block";
+
+			if (type === "error") {
+				toast.querySelector(".toast-body strong").textContent =
+					"Please select a CSV File of Size <= 2MB ❌";
+			}
+			if (type === "success") {
+				toast.querySelector(".toast-body strong").textContent =
+					"File has been uploaded successfully 🔥";
+			}
+
+			toastContainer.appendChild(toast);
+
+			const Toast = new bootstrap.Toast(toast);
+			Toast.show();
+
+			setTimeout(function () {
+				toast.remove();
+			}, 6500);
+		}
+
 		function fileUpload() {
+			let END = false;
+			notification(END, "error");
+			notification(END, "success");
+
 			const btn = document.querySelector(".upload-wrapper > .upload-btn");
 			const file = document.querySelector(".upload-wrapper > input");
+			const form = document.querySelector(".upload > form");
+			const filename = document.querySelector(".filename > span");
 
 			file.addEventListener("change", handleFiles, false);
-			function handleFiles() {
-				const fileList = this.files;
+			function handleFiles(e) {
+				const fileList = this.files; // e.target.files;
 				btn.textContent = fileList[0].name;
-                btn.classList.add("active");
+				btn.classList.add("active");
+			}
+
+			form.addEventListener("submit", uploadFiles, false);
+			async function uploadFiles(e) {
+				e.preventDefault();
+				e.stopPropagation();
+
+				if (file === null) notification(END, "error");
+				if (END) return;
+				if (file === "null") notification(END, "error");
+				if (END) return;
+				if (file === undefined) notification(END, "error");
+				if (END) return;
+				if (file === "undefined") notification(END, "error");
+				if (END) return;
+				if (file === "") notification(END, "error");
+				if (END) return;
+
+				//File Type must be CSV
+				if (file.type.match("text/csv")) {
+					//File Size must be less than equal to 2MB
+					if (file.size <= 1024 * 1024 * 2) {
+						notification(END, "success");
+						try {
+							let formSelf = e.target; //or formSelf = this;
+							let formData = new FormData(formSelf);
+							const response = await fetch("/upload", {
+								method: "POST",
+								body: formData,
+							});
+							const data = await response.json();
+							filename.textContent = data.data.name;
+						} catch (error) {
+							console.log(error);
+						}
+					} else {
+						notification(END, "error");
+					}
+				}
+				if (END) return;
 			}
 		}
 		fileUpload();
