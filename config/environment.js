32d6cb613265a7ad7ca4f7e86ec0fe51e1c22c0c
Ayush <ayush.kanduri@gmail.com>
2022-09-03 21:30:28 +0530

    Scripts updated, Production Logger Created

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,21 @@
 //Require the Dotenv Library
 const dotenv = require("dotenv").config();
+//Require Path Module for the Directory
+const path = require("path");
+//Require File System Module for the Directory
+const fs = require("fs");
+//Require the Rotating File Stream Module for Logging
+const rfs = require("rotating-file-stream");
+
+//Log Directory
+const logDirectory = path.join(__dirname, "../production_logs");
+//Ensure Log Directory exists, if not, Create it
+fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+//User is accessing our Website, so, Access Log Stream
+const accessLogStream = rfs.createStream("access.log", {
+	interval: "1d",
+	path: logDirectory,
+});
 
 //Development Environment
 const development = {
